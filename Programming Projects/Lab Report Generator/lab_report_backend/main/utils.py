4227a48cbf0564cb1a4f73e4259065c43eb0eb39
Nathan Sia <siatzeyuan@gmail.com>
2024-10-23 13:29:50 +0100

    Add the API key and connect backend and frontend

--- Programming Projects/Lab Report Generator/lab_report_backend/main/utils.py
+++ Programming Projects/Lab Report Generator/lab_report_backend/main/utils.py
@@ -1,4 +1,8 @@
 import PyPDF2
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+
 
 def read_pdf(file):
     reader = PyPDF2.PdfReader(file)
@@ -6,3 +10,29 @@ def read_pdf(file):
     for page in range(len(reader.pages)):
         text += reader.pages[page].extract_text()
     return text
+
+def GPT_return_text(question):
+    load_dotenv()
+
+    genai.configure(api_key=os.environ["API_KEY"])
+
+    # Create the model
+    generation_config = {
+    "temperature": 1,
+    "top_p": 0.95,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+    model_name="gemini-1.5-flash",
+    generation_config=generation_config,
+    )
+
+    chat_session = model.start_chat(
+    history=[
+    ]
+    )
+    response = chat_session.send_message(question)
+    return response.text
